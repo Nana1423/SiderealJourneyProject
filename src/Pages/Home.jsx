944a1b24194bf60ac06f7544aeec7bce203e4877
Emanuel Leite <emanuel.leite10@outlook.com>
2023-10-08 16:58:36 -0600

    Adding Routes
    - Login Added
    - Home Page Added
    - Config Added

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,26 @@
+import "../Styles/home.css"
+
+// Components
+import Header from "../Components/Header"
+import Button from '@mui/material/Button';
+import BackgroundVideo from "../Components/BackgroundVideo";
+
+// Video
+import HomeVideo from "../../public/Eclipse_Video.mp4" 
+
+export default function Home() {
+    return (
+        <>
+            <Header />
+            <main className="home">
+                <section className="home-section">
+                    <h1>Hello, Astronauts</h1>
+                    <h2>Let’s discover the eclipses</h2>
+                    <Button variant="contained">Click here to start your journey!</Button>
+                </section>
+                <p>Developed by Nemesis Crew</p>
+            </main>
+            <BackgroundVideo video={HomeVideo}/>
+        </>
+    )
+}
